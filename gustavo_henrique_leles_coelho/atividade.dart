d726233ddf260d9e5f343597608727d06f9df12c
Gustavo Henrique Leles <97555887+GustHenrique@users.noreply.github.com>
2024-08-03 09:10:18 -0300

    Update atividade.dart

--- gustavo_henrique_leles_coelho/atividade.dart
+++ gustavo_henrique_leles_coelho/atividade.dart
@@ -1,8 +1,8 @@
 void main() {
     int numero1, numero2, ano, seculoEncontrado;
-    int resultadoSomaDoisNumeros = calculaDoisNumeros(5, 5);
+    int resultadoSomaDoisNumeros = somaDoisNumeros(5, 5);
     print("O resultado da soma de dois numeros é igual a: ${resultadoSomaDoisNumeros}");
-    seculoEncontrado = retornaSeculo(2024);
+    seculoEncontrado = retornarSeculo(2024);
     print("O ano ${ano} está no ${seculoEncontrado}");
 }
 
