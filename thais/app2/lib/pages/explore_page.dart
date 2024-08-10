d85f28789a59128e6aabccfdc238851a9384856a
ThaisMK <thais.konishi@gmail.com>
2024-08-10 17:17:21 -0300

    explore page

--- thais/app2/lib/pages/explore_page.dart
+++ thais/app2/lib/pages/explore_page.dart
@@ -1,19 +1,48 @@
+import 'package:app2/api/explore_data.dart';
 import 'package:app2/components/restaurant_section.dart';
 import 'package:flutter/material.dart';
 
 import '../models/restaurant.dart';
 
-class ExplorePage extends StatelessWidget {
+class ExplorePage extends StatefulWidget {
   const ExplorePage({super.key});
 
   @override
+  State<ExplorePage> createState() => _ExplorePageState();
+}
+
+class _ExplorePageState extends State<ExplorePage> {
+
+  late MockService mockService;
+
+  @override
+  void initState() {
+
+    super.initState();
+    mockService = MockService();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return ListView(
-      shrinkWrap: true,
-      scrollDirection: Axis.vertical,
-      children: [
-        RestaurantSection(restaurants: restaurants),
-      ],
-    );
+    return FutureBuilder(future: mockService.getExploreData(), builder: (context,snapshot){
+      if(snapshot.connectionState == ConnectionState.done){
+        final restaurants = snapshot.data?.restaurants ?? [];
+        final categories = snapshot.data?.categories ?? [];
+        final posts = snapshot.data?.friendPosts ?? [];
+
+        return ListView(
+          shrinkWrap: true,
+          scrollDirection: Axis.vertical,
+          children: [
+            RestaurantSection(restaurants: restaurants),
+          ],
+        );
+      } else{
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      }
+    });
+
   }
 }
