c693f5fd3b4f0ab1809136c195fd066c041c98f9
Diego Lélis <diegollelis@gmail.com>
2024-08-10 09:08:59 -0300

    create applogin

---
+++ diego-lelis/applogin/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:applogin/login_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purpleAccent),
+        useMaterial3: true,
+      ),
+      home: const LoginPage(),
+    );
+  }
+}
