d0c72c5186877a2c936f6a91ec84c812108919c6
Jeferson Marques Rodrigues <joedf1006@dinfor.uniceub>
2024-08-10 17:16:00 -0300

    exercicios

---
+++ Jeferson Marques/exercicios_primeira_semana/lib/atividade2.dart
@@ -0,0 +1,11 @@
+//Escreva uma função que receba um ano (int) e retorne o século daquele ano. (int)
+
+void main() {
+  int ano = 2001;
+
+  int saberSeculo(int ano) {
+    return (ano / 100).ceil();
+  }
+
+  print(saberSeculo(ano));
+}
