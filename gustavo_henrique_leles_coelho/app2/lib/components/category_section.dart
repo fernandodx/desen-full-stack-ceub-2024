1a87db4e724a920e28a3aa21b443d42b773d2140
julia.aguiar <julia.aguiar@lightbase.com.br>
2024-08-18 18:12:46 -0300

    Adicionando projeto a minha pasta

---
+++ gustavo_henrique_leles_coelho/app2/lib/components/category_section.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/widgets.dart';
+import 'package:app2/models/models.dart';
+import 'package:flutter/material.dart';
+
+class CategorySection extends StatelessWidget {
+  final List<FoodCategory> categories;
+
+  const CategorySection({super.key, required this.categories});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          "Categorias",
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+        SizedBox(
+          height: 230,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: categories.length,
+            itemBuilder: (context, index) {
+              final category = categories[index];
+
+              return SizedBox(
+                width: 300,
+                child: Card(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.vertical(top: Radius.circular(8)),
+                        child: AspectRatio(
+                          aspectRatio: 2,
+                          child: Stack(
+                            fit: StackFit.expand,
+                            children: [
+                              Container(
+                                child: Image.asset(
+                                  category.imageUrl,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              Positioned(
+                                  top: 4.0,
+                                  right: 4.0,
+                                  child: IconButton(
+                                    onPressed: () {},
+                                    icon: const Icon(
+                                      Icons.favorite,
+                                    ),
+                                    iconSize: 30,
+                                    color: Colors.redAccent,
+                                  ))
+                            ],
+                          ),
+                        ),
+                      ),
+                      ListTile(
+                        title: Text(
+                          category.name,
+                          style: Theme.of(context).textTheme.titleSmall,
+                        ),
+                        subtitle: Text(
+                          category.numberOfRestaurants.toString() + " restaurantes",
+                          style: Theme.of(context).textTheme.bodySmall,
+                        ),
+                        onTap: () {
+                          print("Clicou em ${category.name}");
+                        },
+                      )
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        )
+      ],
+    );
+  }
+}
+
